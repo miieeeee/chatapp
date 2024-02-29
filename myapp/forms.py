5fd6d7c357b89da98d09a75eaf384caf81968a95
ryoma_taniguchi <miiekuinfo@gmail.com>
2024-02-29 13:50:07 +0900

    課題２途中

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,40 +1,25 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import Talk
 
 User = get_user_model()
 
 class SignUpForm(UserCreationForm):
-    # img = forms.FileField()
-    username = forms.CharField(label='Username',help_text='')
-    email = forms.EmailField(label="Email address", required=True)
-    password1 = forms.CharField(    
-                                    label=("Password"),
-                                    widget=forms.PasswordInput,
-                                    help_text=''
-                                )
-    password2 = forms.CharField(
-                                    label=("Password confirmation"),
-                                    widget=forms.PasswordInput,
-                                    help_text=("")
-                                )
-    
     class Meta:
         model = User
         fields = ('username','email','password1','password2','icon')
+        labels = {'username':'Username', 'email':'Email address', 'password1':"Password", 'password2':'Password confirmation'}
 
 
 class LoginForm(AuthenticationForm):
     pass
 
-class MessageForm(forms.Form):
-    message = forms.CharField(
-        label="message",
-        max_length=1000,
-        required=True,
-        widget=forms.TextInput()
-    )
-
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Talk
+        fields = ('message',)
+        widgets = {"message":forms.TextInput()}
 
 class UsernameForm(forms.ModelForm):
     class Meta:
--- myapp/views.py
+++ myapp/views.py
@@ -3,23 +3,20 @@ from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm,LoginForm,MessageForm,UsernameForm,EmailForm,IconForm
 from .models import Talk
 from django.contrib.auth.views import LoginView,PasswordChangeView,LogoutView
+from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.utils import timezone
 User = get_user_model()
 def index(request):
-    if(request.user.is_authenticated):print(request.user.id)
-    else : print("nobody logins")
     return render(request, "myapp/index.html")
 
 def signup_view(request):
     error = ''
     if request.method == 'POST':
         form = SignUpForm(request.POST,request.FILES)
-        print(request.POST)
         if form.is_valid():
-            print("success")
             form.save()
             return redirect('index')
         else:
@@ -30,48 +27,47 @@ def signup_view(request):
         form = SignUpForm()
     return render(request, 'myapp/signup.html', {'form' : form,'error_message':error}) 
 
-def login_view(request):
-    return render(request, "myapp/login.html")
-
+@login_required
 def friends(request):
-    friends = User.objects.all()
+    friends = User.objects.exclude(id=request.user.id)
     users = []
     users_no_message = []
     users_have_message = []
     for friend in friends:
-        if(friend.id == request.user.id) : continue
         message = Talk.objects.filter(Q(send_from=request.user.id,send_to=friend.id) | Q(send_from=friend.id,send_to=request.user.id)).order_by('date').last()
         if(message) : users_have_message.append({"id":friend.id,"icon":friend.icon,"username":friend.username,"date":message.date,"message":message.message})
         else : users_no_message.append({"id":friend.id ,"icon":friend.icon,"username":friend.username,"date":(friend.date_joined),"message":None})
     users_have_message = sorted(users_have_message,key=lambda x:x["date"],reverse=True)
     users_no_message = sorted(users_no_message,key=lambda x:x["date"],reverse=True)
-    users.extend(users_have_message)
-    users.extend(users_no_message)
+    for user in users_have_message:
+        users.append(user)
+    for user in users_no_message:
+        users.append(user)
     return render(request, "myapp/friends.html", {'users' : users})
 
+@login_required
 def talk_room(request,pk):
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if(form.is_valid()):
-            Talk.objects.create(
-                send_from = request.user.id,
-                send_to = pk,
-                message = form.cleaned_data['message'],
-                date = timezone.now()
-            )
+            talk = form.save(commit=False)
+            talk.send_from = request.user.id
+            talk.send_to = pk
+            talk.save()
     form = MessageForm()
     talks = Talk.objects.filter(Q(send_from=request.user.id,send_to=pk) | Q(send_from=pk,send_to=request.user.id)).order_by("date")
     return render(request, "myapp/talk_room.html",{"form":form, "pk":pk,"user":{"to" : User.objects.get(id=pk).username,"from":request.user.username},"talks":talks})
 
+@login_required
 def setting(request):
     return render(request, "myapp/setting.html")
-
+ 
 class Login(LoginView):
     authentication_form = LoginForm
     template_name = "myapp/login.html"
     next_page = "friends"
 
- 
+@login_required 
 def username_change(request):
     if(request.method == 'POST'):
         form = UsernameForm(request.POST,instance=request.user)
@@ -81,9 +77,12 @@ def username_change(request):
     else:
         form = UsernameForm()
     return render(request, "myapp/username_change.html",{"form":form})
+
+@login_required
 def username_change_done(request):
     return render(request,"myapp/username_change_done.html")
 
+@login_required
 def email_change(request):
     if(request.method == 'POST'):
         form = EmailForm(request.POST,instance=request.user)
@@ -93,9 +92,12 @@ def email_change(request):
     else:
         form = EmailForm()
     return render(request, "myapp/email_change.html",{"form":form})
+
+@login_required
 def email_change_done(request):
     return render(request,"myapp/email_change_done.html")
 
+@login_required
 def icon_change(request):
     if(request.method == 'POST'):
         form = IconForm(request.POST,request.FILES,instance=request.user)
@@ -105,18 +107,25 @@ def icon_change(request):
     else:
         form = IconForm()
     return render(request, "myapp/icon_change.html",{"form":form})
+
+@login_required
 def icon_change_done(request):
     return render(request,"myapp/icon_change_done.html")
 
+@login_required
 def password_change(request):
     return render(request, "myapp/password_change.html")
+
+@login_required
 def password_change_done(request):
     return render(request,"myapp/password_change_done.html")
 
-class PasswordChange(PasswordChangeView):
+@login_required
+class PasswordChange(LoginRequiredMixin,PasswordChangeView):
     template_name = "myapp/password_change.html"
     success_url = "password_change_done"
 
+@login_required
 class Logout(LoginRequiredMixin,LogoutView):
     template_name = "myapp/index.html"
     next_page = "index"
